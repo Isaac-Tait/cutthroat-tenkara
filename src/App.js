e8451dfb7b132e6ac740c3e9fa82f6fd7636e067
Isaac Tait <isaac@mountaintopcoding.com>
2020-04-21 07:05:38 -0700

    fixed syntax errors

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-        <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" />
+        <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" alt="logo" />
         <h6>
-          After much inner turmoil I am reluctantly stepping away from running Cutthroat Tenkara. The cost in both time and money 💰 to run this small site was too great to justify.
+          After much inner turmoil I am reluctantly stepping away from running Cutthroat Tenkara. The cost in both time and money to run this small site was too great to justify.
           I have a lot of projects on my plate. Maybe eventually though I will resurrect Cutthroat Tenkara. Perhaps one day when I am able to go tenkara fly fishing more than a few times a year...
         </h6>
         <br />
