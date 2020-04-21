2f94c8d769eefa4c9f7eb0e09ef5840e6018a29c
Isaac Tait <isaac@mountaintopcoding.com>
2020-04-21 07:16:08 -0700

    tweaked wording

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
     <div>
         <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" alt="logo" className="image"/>
         <h6>
-          After much inner turmoil I am reluctantly stepping away from running Cutthroat Tenkara. The cost in both time and money to run this small site was too great to justify.
-          I have a lot of projects on my plate. Maybe eventually though I will resurrect Cutthroat Tenkara. Perhaps one day when I am able to go tenkara fly fishing more than a few times a year...
+          After much inner turmoil I am reluctantly relegating Cutthroat Tenkara to the back burner. 
+          It was a grand idea that never really took off...
+          The cost in both time and money to run this small site was also too great to justify.
+          <br />
+          I have a lot of projects on my plate. Perhaps maybe I will eventually resurrect Cutthroat Tenkara.  
+          That eventuality will be contingent on me being able to go tenkara fly fishing more than a few times a year...
         </h6>
         <br />
         <br />
