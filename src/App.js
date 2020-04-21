90e8aec8baec204a69debcfbebd4409a9014dd1e
Isaac Tait <isaac@mountaintopcoding.com>
2020-04-21 07:11:30 -0700

    added style.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-        <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" alt="logo" />
+        <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" alt="logo" className="image"/>
         <h6>
           After much inner turmoil I am reluctantly stepping away from running Cutthroat Tenkara. The cost in both time and money to run this small site was too great to justify.
           I have a lot of projects on my plate. Maybe eventually though I will resurrect Cutthroat Tenkara. Perhaps one day when I am able to go tenkara fly fishing more than a few times a year...
