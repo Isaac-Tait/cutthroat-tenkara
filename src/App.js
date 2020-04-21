9ec80d1cd4559a3c8f8c2d47c9ef0b25714b5c6e
Isaac Tait <isaac@mountaintopcoding.com>
2020-04-21 07:04:51 -0700

    edited home page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" />
+        <h6>
+          After much inner turmoil I am reluctantly stepping away from running Cutthroat Tenkara. The cost in both time and money 💰 to run this small site was too great to justify.
+          I have a lot of projects on my plate. Maybe eventually though I will resurrect Cutthroat Tenkara. Perhaps one day when I am able to go tenkara fly fishing more than a few times a year...
+        </h6>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>Yet another <a href="https://www.mountaintopcoding.com">mountainTopCoding(&#9968;);</a> project </p>
     </div>
   );
 }
