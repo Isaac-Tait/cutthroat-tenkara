dca4cdac29b364683c9f68a85aa2a176f384623b
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-19 12:35:20 -0500

    here we go again 👀

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div>
         <img src="https://cutthroat-tenkara.s3.amazonaws.com/CtT.jpg" alt="logo" className="image"/>
-        <h6>
+        <p>
           After much inner turmoil I am reluctantly relegating Cutthroat Tenkara to the back burner. 
           It was a grand idea that never really took off...
           The cost in both time and money to run this small site was also too great to justify.
           <br />
           I have a lot of projects on my plate. Perhaps maybe I will eventually resurrect Cutthroat Tenkara.  
           That eventuality will be contingent on me being able to go tenkara fly fishing more than a few times a year...
-        </h6>
+        </p>
         <br />
         <br />
         <br />
